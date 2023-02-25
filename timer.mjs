1057ba7b5882f063382c02f478ae6a8cafbd5add
Xunnamius (Remus) <Xunnamius@users.noreply.github.com>
2023-02-25 15:37:43 -0800

    implement new cool features for timer.mjs

--- timer.mjs
+++ timer.mjs
@@ -16,15 +16,145 @@
  * day is greater than 9.
  *
  * Once the script is running, you can use the keyboard to Pause the timer,
- * Resume the timer, Fast-forward the timer to a specific time and day, and get
- * information about the Next notification times.
+ * Resume the timer, Time-travel to a specific time and day, and get information
+ * about the Next notification times.
  */
 
 import { playAudioFile } from 'audic';
 import readline from 'readline';
 
-const notificationSoundFileLifeAndLiving = 'notification-lal.mp3';
-const notificationSoundFileTrelai = 'notification-trelai.mp3';
+// * The game's default start time is 09:00
+let startTimeHours = 9;
+let startTimeMinutes = 0;
+
+// ! If you add more channels, you should also add alarmFiles and channels entries
+const channelLifeAndLiving = Symbol('life-and-living');
+const channelTrelaiHqTv = Symbol('trelai-hq-tv');
+
+// ! Must correspond to entries in alarmFiles and the channelX constants
+const channels = {
+  [channelLifeAndLiving]: { name: 'Life and Living' },
+  [channelTrelaiHqTv]: { name: 'Trelai HQ TV' }
+};
+
+// ! Must be paths relative to the timer.mjs file
+const alarmFiles = {
+  [channelLifeAndLiving]: 'notification-lal.mp3',
+  [channelTrelaiHqTv]: 'notification-trelai.mp3'
+};
+
+// ! Must be in ascending chronological order
+const alarmEvents = [
+  // * 9am is the default start time, so catch that cooking show!
+  { day: 1, time: '09:00', channel: channelLifeAndLiving },
+  { day: 1, time: '11:50', channel: channelLifeAndLiving },
+  { day: 1, time: '12:00', channel: channelLifeAndLiving },
+  { day: 1, time: '12:50', channel: channelTrelaiHqTv },
+  { day: 1, time: '13:00', channel: channelTrelaiHqTv },
+  { day: 1, time: '16:50', channel: channelTrelaiHqTv },
+  { day: 1, time: '17:00', channel: channelTrelaiHqTv },
+  { day: 1, time: '17:20', channel: channelTrelaiHqTv },
+  { day: 1, time: '17:30', channel: channelTrelaiHqTv },
+  { day: 1, time: '17:50', channel: channelLifeAndLiving },
+  { day: 1, time: '18:00', channel: channelLifeAndLiving },
+
+  { day: 2, time: '05:50', channel: channelLifeAndLiving },
+  { day: 2, time: '06:00', channel: channelLifeAndLiving },
+  { day: 2, time: '06:20', channel: channelTrelaiHqTv },
+  { day: 2, time: '06:30', channel: channelTrelaiHqTv },
+  { day: 2, time: '06:50', channel: channelTrelaiHqTv },
+  { day: 2, time: '07:00', channel: channelTrelaiHqTv },
+  { day: 2, time: '11:50', channel: channelLifeAndLiving },
+  { day: 2, time: '12:00', channel: channelLifeAndLiving },
+  { day: 2, time: '17:50', channel: channelLifeAndLiving },
+  { day: 2, time: '18:00', channel: channelLifeAndLiving },
+
+  { day: 3, time: '05:50', channel: channelLifeAndLiving },
+  { day: 3, time: '06:00', channel: channelLifeAndLiving },
+  { day: 3, time: '06:20', channel: channelTrelaiHqTv },
+  { day: 3, time: '06:30', channel: channelTrelaiHqTv },
+  { day: 3, time: '11:50', channel: channelLifeAndLiving },
+  { day: 3, time: '12:00', channel: channelLifeAndLiving },
+  { day: 3, time: '17:50', channel: channelLifeAndLiving },
+  { day: 3, time: '18:00', channel: channelLifeAndLiving },
+
+  // * Days 4 and 8 are missing the 6pm show on LaL
+  { day: 4, time: '05:50', channel: channelLifeAndLiving },
+  { day: 4, time: '06:00', channel: channelLifeAndLiving },
+  { day: 4, time: '06:20', channel: channelTrelaiHqTv },
+  { day: 4, time: '06:30', channel: channelTrelaiHqTv },
+  { day: 4, time: '06:50', channel: channelTrelaiHqTv },
+  { day: 4, time: '07:00', channel: channelTrelaiHqTv },
+  { day: 4, time: '11:50', channel: channelLifeAndLiving },
+  { day: 4, time: '12:00', channel: channelLifeAndLiving },
+  { day: 4, time: '16:50', channel: channelTrelaiHqTv },
+  { day: 4, time: '17:00', channel: channelTrelaiHqTv },
+
+  { day: 5, time: '05:50', channel: channelLifeAndLiving },
+  { day: 5, time: '06:00', channel: channelLifeAndLiving },
+  { day: 5, time: '06:20', channel: channelTrelaiHqTv },
+  { day: 5, time: '06:30', channel: channelTrelaiHqTv },
+  { day: 5, time: '11:50', channel: channelLifeAndLiving },
+  { day: 5, time: '12:00', channel: channelLifeAndLiving },
+  { day: 5, time: '13:20', channel: channelTrelaiHqTv },
+  { day: 5, time: '13:30', channel: channelTrelaiHqTv },
+  { day: 5, time: '17:50', channel: channelLifeAndLiving },
+  { day: 5, time: '18:00', channel: channelLifeAndLiving },
+
+  { day: 6, time: '05:50', channel: channelLifeAndLiving },
+  { day: 6, time: '06:00', channel: channelLifeAndLiving },
+  { day: 6, time: '06:20', channel: channelTrelaiHqTv },
+  { day: 6, time: '06:30', channel: channelTrelaiHqTv },
+  { day: 6, time: '06:50', channel: channelTrelaiHqTv },
+  { day: 6, time: '07:00', channel: channelTrelaiHqTv },
+  { day: 6, time: '07:20', channel: channelTrelaiHqTv },
+  { day: 6, time: '07:30', channel: channelTrelaiHqTv },
+  { day: 6, time: '11:50', channel: channelLifeAndLiving },
+  { day: 6, time: '12:00', channel: channelLifeAndLiving },
+  { day: 6, time: '17:50', channel: channelLifeAndLiving },
+  { day: 6, time: '18:00', channel: channelLifeAndLiving },
+
+  { day: 7, time: '05:50', channel: channelLifeAndLiving },
+  { day: 7, time: '06:00', channel: channelLifeAndLiving },
+  { day: 7, time: '06:50', channel: channelTrelaiHqTv },
+  { day: 7, time: '07:00', channel: channelTrelaiHqTv },
+  { day: 7, time: '07:50', channel: channelTrelaiHqTv },
+  { day: 7, time: '08:00', channel: channelTrelaiHqTv },
+  { day: 7, time: '11:50', channel: channelLifeAndLiving },
+  { day: 7, time: '12:00', channel: channelLifeAndLiving },
+  { day: 7, time: '12:50', channel: channelTrelaiHqTv },
+  { day: 7, time: '13:00', channel: channelTrelaiHqTv },
+  { day: 7, time: '17:50', channel: channelLifeAndLiving },
+  { day: 7, time: '18:00', channel: channelLifeAndLiving },
+
+  // * Days 4 and 8 are missing the 6pm show on LaL
+  { day: 8, time: '05:50', channel: channelLifeAndLiving },
+  { day: 8, time: '06:00', channel: channelLifeAndLiving },
+  { day: 8, time: '06:50', channel: channelTrelaiHqTv },
+  { day: 8, time: '07:00', channel: channelTrelaiHqTv },
+  { day: 8, time: '07:20', channel: channelTrelaiHqTv },
+  { day: 8, time: '07:30', channel: channelTrelaiHqTv },
+  { day: 8, time: '11:50', channel: channelLifeAndLiving },
+  { day: 8, time: '12:00', channel: channelLifeAndLiving },
+  { day: 8, time: '12:50', channel: channelTrelaiHqTv },
+  { day: 8, time: '13:00', channel: channelTrelaiHqTv },
+
+  // * Media blackout begins
+  { day: 9, time: '05:50', channel: channelLifeAndLiving },
+  { day: 9, time: '06:00', channel: channelLifeAndLiving },
+  { day: 9, time: '06:20', channel: channelTrelaiHqTv },
+  { day: 9, time: '06:30', channel: channelTrelaiHqTv },
+  { day: 9, time: '07:20', channel: channelTrelaiHqTv },
+  { day: 9, time: '07:30', channel: channelTrelaiHqTv },
+
+  { day: 10, time: '06:20', channel: channelTrelaiHqTv },
+  { day: 10, time: '06:30', channel: channelTrelaiHqTv },
+
+  { day: 18, time: '12:20', channel: channelTrelaiHqTv },
+  { day: 18, time: '12:30', channel: channelTrelaiHqTv }
+];
+
+// *** End of configurable globals ***
 
 const igMinutesPerIrlSecond = 24 / 60;
 const startTimeArgv = process.argv[2];
@@ -32,8 +162,6 @@ let startDayArgv = process.argv[3];
 
 let interval = null;
 let startTimeMs = Date.now();
-let startTimeHours = 9;
-let startTimeMinutes = 0;
 let pauseTimeMs = 0;
 
 let seenInput = '';
@@ -48,7 +176,7 @@ if (startTimeArgv) {
     Number.isNaN(hours) ||
     Number.isNaN(minutes)
   ) {
-    throw new Error('invalid start time');
+    throw new Error(`invalid start time (${startTimeArgv})`);
   }
 
   startTimeHours = hours;
@@ -78,7 +206,7 @@ process.stdin.on('keypress', (_, key) => {
         Number.isNaN(minutes) ||
         Number.isNaN(day)
       ) {
-        throw new Error('invalid input format');
+        throw new Error(`invalid input (${seenInput})`);
       }
 
       startTimeMs = Date.now();
@@ -92,18 +220,24 @@ process.stdin.on('keypress', (_, key) => {
       process.stdout.cursorTo(0, 0);
       process.stdout.clearScreenDown();
       restartProgram();
-    } else {
-      const keyValue = key.name === 'space' ? ' ' : key.name;
-      process.stdout.write(keyValue);
-      seenInput += keyValue;
+    } else if (key.name === 'backspace' && seenInput.length) {
+      process.stdout.moveCursor(-1, 0);
+      process.stdout.clearLine(1);
+      seenInput = seenInput.slice(0, -1);
+    } else if (key.name === 'space') {
+      process.stdout.write(' ');
+      seenInput += ' ';
+    } else if (/^[0-9]$/.test(key.name)) {
+      process.stdout.write(key.name);
+      seenInput += key.name;
     }
   } else {
-    if (key.name === 'p' || key.name === 'f') {
+    if ((key.name === 'p' || key.name === 't') && pauseTimeMs <= 0) {
       clearInterval(interval);
       pauseTimeMs = Date.now();
       clearLineAndWrite(`-- clock paused at ${getTime({ format: 2 })} --`);
 
-      if (key.name === 'f') {
+      if (key.name === 't') {
         process.stdout.write('\nenter new time and day (HH MM D): ');
         lookingForInput = true;
       }
@@ -111,12 +245,28 @@ process.stdin.on('keypress', (_, key) => {
       startTimeMs += Date.now() - pauseTimeMs;
       pauseTimeMs = 0;
       startReportingTime();
-    } else if (key.name === 'n') {
-      clearLineAndWrite(`--> upcoming notifications <--\n`);
-      process.stdout.write(`${peekNextAlarm(0)}\n`);
-      process.stdout.write(`${peekNextAlarm(1)}\n`);
-      process.stdout.write(`${peekNextAlarm(2)}\n`);
-      process.stdout.write('--\n');
+    } else if (key.name === 'n' && pauseTimeMs <= 0) {
+      const [_, time, day] = getTime({ format: 3 });
+
+      clearLineAndWrite(
+        `--> upcoming notifications as of ${getTime({ format: 2 })} <--\n`
+      );
+
+      const peek1Ahead = peekNextAlarm(time, day, 0);
+      const peek2Ahead = peekNextAlarm(time, day, 1);
+      const peek3Ahead = peekNextAlarm(time, day, 2);
+
+      process.stdout.write(`${peek1Ahead}\n`);
+
+      if (peek2Ahead !== '(none)') {
+        process.stdout.write(`${peek2Ahead}\n`);
+      }
+
+      if (peek3Ahead !== '(none)') {
+        process.stdout.write(`${peek3Ahead}\n`);
+      }
+
+      process.stdout.write(`---\n${getTime({ format: 1 })}`);
     }
   }
 });
@@ -132,7 +282,7 @@ function initializeScreen() {
   console.log(
     `started timer at ${pad(startTimeHours)}:${pad(startTimeMinutes)} on day ${
       startDayArgv ?? 1
-    }\npress "P" to pause\npress "R" to resume\npress "F" to fast-forward\npress "CTRL+C" to quit\n---`
+    }\npress "P" to pause\npress "R" to resume\npress "T" to time-travel\npress "N" to see upcoming notifications\npress "CTRL+C" to quit\n---`
   );
 
   process.stdout.write('initializing...');
@@ -153,6 +303,12 @@ function startReportingTime() {
 }
 
 function getTime({ format = 1 } = {}) {
+  if (startTimeMinutes < 0 || startTimeMinutes > 59) {
+    throw new Error(`invalid start time minutes (${startTimeHours})`);
+  } else if (startTimeHours < 0 || startTimeHours > 23) {
+    throw new Error(`invalid start time hours (${startTimeHours})`);
+  }
+
   const igMinutesSinceStart =
     ((Date.now() - startTimeMs) / 1000) * igMinutesPerIrlSecond +
     startTimeMinutes +
@@ -168,8 +324,8 @@ function getTime({ format = 1 } = {}) {
       : (startDayArgv ? Number(startDayArgv) : 1) +
         Math.floor(igMinutesSinceStart / (60 * 24));
 
-  if (Number.isNaN(startTimeArgv)) {
-    throw new Error('invalid start day');
+  if (Number.isNaN(day) || day < 1) {
+    throw new Error(`invalid start day (${startDayArgv})`);
   }
 
   /**
@@ -205,65 +361,59 @@ function clearLineAndWrite(str) {
  * @param day {number}
  */
 function maybeTriggerAlarm(time, day) {
-  const [subject, alarmTime, alarmDay] = getNextAlarm();
+  const subject = getMatchingAlarm(time, day);
 
-  if (day === alarmDay && time === alarmTime) {
-    playAudioFile(
-      subject === 'life-and-living'
-        ? notificationSoundFileLifeAndLiving
-        : notificationSoundFileTrelai
-    );
+  if (subject) {
+    playAudioFile(alarmFiles[subject]);
   }
-}
-
-function getNextAlarm() {
-  // *** Life and Living channel schedule alarm***
-  if (
-    // * 9am is the default start time, so catch that cooking show!
-    (day === 1 && time === '09:00') ||
-    // * Skip the 6pm time slot on the 4th and 8th days
-    (day < 9 && day !== 4 && day !== 8 && ['17:50', '18:00'].includes(time)) ||
-    // * Issue notifications until the media blackout occurs
-    (day < 9 && ['05:50', '06:00', '11:50', '12:00'].includes(time)) ||
-    (day === 9 && ['05:50', '06:00'].includes(time))
-  ) {
-    // TODO
-  }
-
-  // *** Trelai HQ TV channel schedule alarm***
-  if (
-    (day === 1 &&
-      ['12:50', '13:00', '16:50', '17:00', '17:20', '17:30'].includes(time)) ||
-    (day === 2 && ['06:20', '06:30', '06:50', '07:00'].includes(time)) ||
-    (day === 3 && ['06:20', '06:30'].includes(time)) ||
-    (day === 4 &&
-      ['06:20', '06:30', '06:50', '07:00', '16:50', '17:00'].includes(time)) ||
-    (day === 5 && ['06:20', '06:30', '13:20', '13:30'].includes(time)) ||
-    (day === 6 &&
-      ['06:20', '06:30', '06:50', '07:00', '07:20', '07:30'].includes(time)) ||
-    (day === 7 &&
-      ['06:50', '07:00', '07:50', '08:00', '12:50', '13:00'].includes(time)) ||
-    (day === 8 &&
-      ['06:50', '07:00', '07:20', '07:30', '12:50', '13:00'].includes(time)) ||
-    (day === 9 &&
-      ['05:50', '06:00', '06:20', '06:30', '07:20', '07:30'].includes(time)) ||
-    (day === 10 && ['06:20', '06:30'].includes(time)) ||
-    (day === 18 && ['12:20', '12:30'].includes(time))
-  ) {
-    // TODO
-  }
-
-  // TODO: returns [subject, alarmTime, alarmDay]
-  return [];
 }
 
 /**
  *
  * @param time {string}
  * @param day {number}
- * @param index {number}
  */
-function peekNextAlarm(time, day, index) {
-  // TODO: returns a string containing "subject at alarmTime" index based on time and day
-  return '';
+function getMatchingAlarm(time, day) {
+  const alarmEvent = alarmEvents.find(({ time: alarmTime, day: alarmDay }) => {
+    return alarmDay === day && alarmTime === time;
+  });
+
+  return alarmEvent?.channel;
+}
+
+/**
+ *
+ * @param time {string}
+ * @param day {number}
+ * @param lookNIndicesAhead {number}
+ */
+function peekNextAlarm(time, day, lookNIndicesAhead) {
+  let nextAlarmString = '(none)';
+  let lastAlarmIndex = -1;
+
+  do {
+    const alarmEvent = alarmEvents.find(
+      ({ time: alarmTime, day: alarmDay }, index) => {
+        const result =
+          (alarmDay > day || (alarmDay == day && alarmTime >= time)) &&
+          index > lastAlarmIndex;
+
+        if (lastAlarmIndex < index) {
+          lastAlarmIndex = index;
+        }
+
+        return result;
+      }
+    );
+
+    if (!alarmEvent) {
+      break;
+    }
+
+    nextAlarmString = `${channels[alarmEvent.channel].name} at ${
+      alarmEvent.time
+    } on day ${alarmEvent.day}`;
+  } while (lookNIndicesAhead-- > 0);
+
+  return nextAlarmString;
 }
