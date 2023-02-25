1e31f4f1326b850e2c232f6fbef391b898f8b73d
robot <robot@robot.robot>
2023-02-25 00:56:23 -0800

    Sat Feb 25 12:56:22 AM PST 2023

--- parse-radio-data.mjs
+++ parse-radio-data.mjs
@@ -1,6 +1,7 @@
 /**
  * This script will take a radio data XML file and output scheduling information
- * about it.
+ * about it. Right now it only outputs a subset of TV broadcast data, but it
+ * could be easily adapted to output radio or other data.
  *
  * You can call this script like so (after running npm install):
  *
@@ -16,52 +17,53 @@ import { Console } from 'node:console';
 import { Transform } from 'node:stream';
 
 const SkillInteractions = {
-  SPR: 'Sprinting',
-  LFT: 'Lightfooted',
-  NIM: 'Nimble',
-  SNE: 'Sneaking',
-  BAA: 'Axe',
-  BUA: 'Blunt',
-  CRP: 'Carpentry',
-  COO: 'Cooking',
-  FRM: 'Farming',
-  DOC: 'Doctor',
-  ELC: 'Electricity',
-  MTL: 'Metalworking',
-  AIM: 'Aiming',
-  REL: 'Reloading',
-  FIS: 'Fishing',
-  TRA: 'Trapping',
-  FOR: 'Foraging',
-  TAI: 'Tailoring',
-  MEC: 'Mechanics',
-  CMB: 'Combat',
-  SPE: 'Spear',
-  SBU: 'SmallBlunt',
-  LBA: 'LongBlade',
-  SBA: 'SmallBlade'
+  SPR: 'sprinting',
+  LFT: 'lightfooted',
+  NIM: 'nimble',
+  SNE: 'sneaking',
+  BAA: 'axe',
+  BUA: 'blunt',
+  CRP: 'carpentry',
+  COO: 'cooking',
+  FRM: 'farming',
+  DOC: 'doctor',
+  ELC: 'electricity',
+  MTL: 'metalworking',
+  AIM: 'aiming',
+  REL: 'reloading',
+  FIS: 'fishing',
+  TRA: 'trapping',
+  FOR: 'foraging',
+  TAI: 'tailoring',
+  MEC: 'mechanics',
+  CMB: 'combat',
+  SPE: 'spear',
+  SBU: 'smallBlunt',
+  LBA: 'longBlade',
+  SBA: 'smallBlade'
 };
 
 const StatInteractions = {
-  ANG: 'Anger',
-  BOR: 'Boredom',
-  END: 'Endurance',
-  FAT: 'Fatigue',
-  FIT: 'Fitness',
-  HUN: 'Hunger',
-  MOR: 'Morale',
-  STS: 'Stress',
-  FEA: 'Fear',
-  PAN: 'Panic',
-  SAN: 'Sanity',
-  SIC: 'Sickness',
-  PAI: 'Pain',
-  DRU: 'Drunkenness',
-  THI: 'Thirst',
-  UHP: 'Unhappiness'
+  ANG: 'anger',
+  BOR: 'boredom',
+  END: 'endurance',
+  FAT: 'fatigue',
+  FIT: 'fitness',
+  HUN: 'hunger',
+  MOR: 'morale',
+  STS: 'stress',
+  FEA: 'fear',
+  PAN: 'panic',
+  SAN: 'sanity',
+  SIC: 'sickness',
+  PAI: 'pain',
+  DRU: 'drunkenness',
+  THI: 'thirst',
+  UHP: 'unhappiness'
 };
 
-const unknown = Symbol('unknown');
+const $unknown = Symbol('unknown');
+const $sort = Symbol('sort');
 const radioDataFilePath = process.argv[2];
 
 if (!radioDataFilePath) {
@@ -86,22 +88,25 @@ const data = (
   ) || []
 ).map((channelEntry) => {
   return {
-    name: channelEntry.$.name || unknown,
+    name: channelEntry.$.name || $unknown,
     scripts: (channelEntry.ScriptEntry || []).map((scriptEntry) => {
       return {
-        name: scriptEntry.$.name || unknown,
+        name: scriptEntry.$.name || $unknown,
         isStartScript:
           scriptEntry.$.name && scriptEntry.$.name === channelEntry.$.name,
         broadcasts: (scriptEntry.BroadcastEntry || []).map((broadcastEntry) => {
           return {
-            day: broadcastEntry.$.day ?? unknown,
+            day:
+              broadcastEntry.$.day === undefined
+                ? $unknown
+                : Number(broadcastEntry.$.day) + 1,
             startTime:
               broadcastEntry.$.timestamp === undefined
-                ? unknown
+                ? $unknown
                 : minutesToTime(broadcastEntry.$.timestamp),
             endTime:
               broadcastEntry.$.endstamp === undefined
-                ? unknown
+                ? $unknown
                 : minutesToTime(broadcastEntry.$.endstamp),
             lines: (broadcastEntry.LineEntry || []).map((lineEntry) => {
               return {
@@ -118,19 +123,19 @@ const data = (
 
                     return {
                       interaction:
-                        skillInteraction || statInteraction || unknown,
+                        skillInteraction || statInteraction || `"${id}"?`,
                       type: skillInteraction
                         ? 'skill'
                         : statInteraction
                         ? 'stat'
-                        : unknown,
+                        : $unknown,
                       operation:
                         op === '+'
                           ? 'addition'
                           : op === '-'
                           ? 'subtraction'
-                          : unknown,
-                      number: num
+                          : $unknown,
+                      number: Number(num)
                     };
                   })
               };
@@ -144,6 +149,8 @@ const data = (
 
 data.forEach((show) => {
   show.scripts.forEach((script) => {
+    let previousDay = 0;
+
     console.log(show.name, '-', script.name);
     console.log('---');
 
@@ -153,44 +160,64 @@ data.forEach((show) => {
       let statInteractions = [];
 
       lines.forEach(({ codes }) => {
+        let addSkillTick = false;
+
         codes.forEach(({ interaction, type, operation, number }) => {
           const interactionsObject =
             type === 'skill' ? skillInteractions : statInteractions;
 
+          addSkillTick ||= type === 'skill';
+
           let target = interactionsObject.find(
-            ({ skill, stat }) => skill === interaction || stat === interaction
+            (obj) => obj[$sort] === interaction
           );
 
           if (target === undefined) {
-            target = { [type]: interaction, change: 0 };
+            target = {
+              [$sort]: interaction,
+              [type === 'skill' ? 'skill' : 'stat']: interaction,
+              change: 0
+            };
+            interactionsObject.push(target);
           }
 
           target.change += (operation === 'subtraction' ? -1 : 1) * number;
         });
+
+        if (addSkillTick) {
+          ticks += 1;
+        }
       });
 
       [...skillInteractions, ...statInteractions].forEach(
         (interactionObject) => {
           interactionObject.change = `${
-            interactionObject.change > 0
-              ? '+'
-              : interactionObject.change < 0
-              ? '-'
-              : ''
-          }${interactionObject}`;
+            interactionObject.change > 0 ? '+' : ''
+          }${interactionObject.change}`;
         }
       );
 
+      skillInteractions = sortInteractions(skillInteractions);
+      statInteractions = sortInteractions(statInteractions);
+
+      let prefix = '';
+
+      if (previousDay && day !== previousDay) {
+        prefix = '---\n\n\n\n\n---\n---\n';
+      }
+
+      previousDay = day;
+
       console.log(
-        `[day ${day}] ${startTime} to ${endTime}\nTicks: ${ticks}\nSkills:${
+        `${prefix}[day ${day}] ${startTime} to ${endTime}\nSkill ticks: ${ticks}\nAffected skills:${
           skillInteractions.length
             ? `\n${getTable(skillInteractions)}`
-            : '(none)\n'
-        }Stats:${
+            : ' (none)'
+        }\nAffected stats:${
           statInteractions.length
             ? `\n${getTable(statInteractions)}`
-            : '(none)\n'
-        }---`
+            : ' (none)'
+        }\n---`
       );
     });
   });
@@ -202,6 +229,16 @@ function minutesToTime(time) {
   )
     .toString()
     .padStart(2, '0')}`;
+}
+
+function sortInteractions(interactions) {
+  return interactions.sort((interactionA, interactionB) => {
+    if (interactionA[$sort].startsWith('"')) return 1;
+    if (interactionB[$sort].startsWith('"')) return -1;
+    if (interactionA[$sort] < interactionB[$sort]) return -1;
+    if (interactionA[$sort] > interactionB[$sort]) return 1;
+    return 0;
+  });
 }
 
 // * https://stackoverflow.com/a/69874540/1367414
@@ -228,5 +265,5 @@ function getTable(input) {
     result += `${r}\n`;
   }
 
-  return result;
+  return result.trim();
 }
